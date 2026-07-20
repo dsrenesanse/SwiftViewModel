8224961614d5ac2043366a1f84e4b14339cbd5fb
dsrenesanse <dsrenesanse@icloud.com>
2026-07-20 12:28:14 +0900

    here we go

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftViewModel",
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +13,15 @@ let package = Package(
             targets: ["SwiftViewModel"]
         ),
     ],
+    dependencies: [.package(url: "https://github.com/dsrenesanse/SwiftClonable", branch: "main")],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftViewModel"
+            name: "SwiftViewModel",
+            dependencies: [
+                .product(name: "SwiftClonable", package: "SwiftClonable"),
+            ]
         ),
         .testTarget(
             name: "SwiftViewModelTests",
--- Sources/SwiftViewModel/SwiftViewModel.swift
+++ Sources/SwiftViewModel/SwiftViewModel.swift
@@ -1,2 +1,83 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Test.swift
+//  Beam
+//
+//  Created by Dan on 2026/02/16.
+//
+
+import SwiftClonable
+import SwiftUI
+
+@Observable
+@MainActor
+class ViewModel<T: Clonable> {
+
+    @ObservationIgnored
+    private var pending: Task<Void, Never>?
+
+    @ObservationIgnored
+    private var pendingStorage = [UUID: Task<Void, Never>]()
+
+    func process(
+        computation: @escaping (_ state: T) async -> (_ state: T) -> Void
+    ) {
+        let key = UUID()
+        let task = Task { [weak self] in
+            guard let copy = (self as? T)?.copy() else { return }
+            let update = await computation(copy)
+            self?.pendingStorage.removeValue(forKey: key)
+            if self != nil {
+                update(self as! T)
+            }
+        }
+        pendingStorage[key] = task
+    }
+
+    func processSync(
+        computation: @escaping (_ state: T) async -> (_ state: T) -> Void
+    ) {
+        let key = UUID()
+        let old = pending
+        pending = Task { [weak self] in
+            await old?.value
+            guard let copy = (self as? T)?.copy() else { return }
+            let update = await computation(copy)
+            self?.pendingStorage.removeValue(forKey: key)
+            if self != nil {
+                update(self as! T)
+            }
+        }
+        pendingStorage[key] = pending
+    }
+
+    deinit {
+        pending?.cancel()
+        let storage = pendingStorage
+        for task in storage.values {
+            task.cancel()
+        }
+    }
+}
+
+public actor SyncProcessor {
+    private var pending = [UUID: Task<Any?, Never>]()
+
+    public func process<T>(action: Task<T, Never>) async -> T? {
+        let key = UUID()
+        let capturedPendings = Array(pending.values)
+        let task = Task<Any?, Never> { [weak self] in
+            guard self != nil else { return nil }
+            let result = await action.value
+            guard self != nil else { return nil }
+            for pending in capturedPendings {
+                let _ = await pending.value
+                guard self != nil else { return nil }
+            }
+            return result
+        }
+        pending[key] = task
+        let result = await task.value
+        pending.removeValue(forKey: key)
+        return (result as? T)
+    }
+}
